bfd11060e48a5631812c1fd761b669a1e2a99894
yongss <bbl737898@gmail.com>
2025-07-28 22:45:46 +0900

    feat: AppConfig.java의 순수 자바 코드를 Spring 코드로 변경
    - @Configuration으로 AppConfig를 설정으로 바꾸기
    - @Bean으로 Spring Container에 등록

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,18 +5,24 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
 
 //        MemberService memberService = new MemberServiceImpl(null);
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = new OrderServiceImpl(null, null);
-        OrderService orderService = appConfig.orderService();
+//        OrderService orderService = appConfig.orderService();
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
